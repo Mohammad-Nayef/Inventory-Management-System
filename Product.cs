a4681bb2b5915eccc86a757b656d74160d039ec4
mohammad-nayef <awladmohammad20@gmail.com>
2023-07-18 19:40:52 +0300

    Added the Product class as Product.cs
    Product.cs contains details about an indivisual product. It has the
    name, price and quantity of a product with setters and getters.

---
+++ Product.cs
@@ -0,0 +1,43 @@
+﻿public class Product
+{
+    private string name;
+    private decimal price;
+    private int quantity;
+
+    public Product(string name, decimal price, int quantity) 
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public decimal Price
+    {
+        get { return price; }
+        set 
+        {
+            if (value < 0)
+                Console.WriteLine("Please try again and enter a non-negative integer.");
+            else 
+                price = value; 
+        }
+    }
+
+    public int Quantity
+    {
+        get { return quantity; }
+        set 
+        {
+            if (value < 0)
+                Console.WriteLine("Please try again and enter a non-negative integer.");
+            else
+                quantity = value; 
+        }
+    }
+}
