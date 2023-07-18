3f80de2fd936cb9861608b8fda6499a714f5d16c
mohammad-nayef <awladmohammad20@gmail.com>
2023-07-18 23:21:11 +0300

    Main.cs and Inventory.cs are added

--- Inventory.cs
+++ Inventory.cs
@@ -2,12 +2,5 @@
 {
     private List<Product> products = new();
 
-    public void Add(string name, decimal price, int quantity)
-    {
-        Product newProduct = new(name, price, quantity);
-        if (Product.AreValid(price, quantity)) 
-        {
-            products.Add(newProduct);
-        }
-    }
+
 }
--- Main.cs
+++ Main.cs
@@ -2,31 +2,3 @@
 string name;
 decimal price; 
 int quantity, option;
-
-while (true)
-{
-    Console.WriteLine("Inventory Management System\n");
-    Console.WriteLine("1. Add a product.");
-    Console.WriteLine("0. Exit.\n");
-    Console.Write("Choose a valid option: ");
-    option = int.Parse(Console.ReadLine());
-
-    switch (option)
-    {
-        case 0:
-            return 0;
-        case 1:
-            Console.Write("Name of the product: ");
-            name = Console.ReadLine();
-            Console.Write("Price of the product: ");
-            price = decimal.Parse(Console.ReadLine());
-            Console.Write("Quantity of the product: ");
-            quantity = int.Parse(Console.ReadLine());
-            inventory.Add(name, price, quantity);
-            break;
-        default:
-            Console.WriteLine("Please, enter a valid option.");
-            break;
-    }
-    Console.Clear();
-}
