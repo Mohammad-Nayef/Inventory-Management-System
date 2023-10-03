78929d40690ad1904dda8e5442ea9fed6b240f06
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-03 21:17:20 +0300

    Created a database connection to Inventory database

--- ProductsDatabase.cs
+++ ProductsDatabase.cs
@@ -2,5 +2,19 @@
 
 public class ProductsDatabase
 {
+    private static readonly Lazy<ProductsDatabase> _lazyInstance = new Lazy<ProductsDatabase>(() => new ProductsDatabase());
+    private static SqlConnection sqlConnection;
+    private string connectionString = "Data Source = MOHAMMAD; " +
+        "Initial Catalog = Inventory; " +
+        "Integrated Security = True;";
+
+    public static ProductsDatabase Instance => _lazyInstance.Value;
+
+    private ProductsDatabase()
+    {
+        sqlConnection = new SqlConnection(connectionString);
+        sqlConnection.Open();
+    }
+
     
 }
