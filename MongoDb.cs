31557482b6610ccc079ea6191c2fb264be67caa2
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-04 17:19:20 +0300

    Created a connection to MongoDb

--- MongoDb.cs
+++ MongoDb.cs
@@ -2,5 +2,18 @@
 
 public class MongoDb
 {
-   
+    private static readonly Lazy<MongoDb> _lazyInstance = new Lazy<MongoDb>(() => new MongoDb());
+    private const string connectionString = "mongodb+srv://moha:Yr2Ab6A2lVO00J0A@cluster0.j4olwzd.mongodb.net/?retryWrites=true&w=majority";
+    private MongoClient client;
+    private IMongoDatabase database;
+    private IMongoCollection<Product> productsCollection;
+
+    public static MongoDb Instance => _lazyInstance.Value;
+
+    private MongoDb()
+    {
+        client = new MongoClient(connectionString);
+        database = client.GetDatabase("inventory");
+        productsCollection = database.GetCollection<Product>("products");
+    }
 }
