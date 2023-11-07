8b0ebb58476cedced8039b4149751ca7cc025dc5
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-07 22:23:07 +0200

    Moved the connection string to constants file

---
+++ Constants.cs
@@ -0,0 +1,5 @@
+﻿public static class Constants
+{
+    public static string MongoDbConnectionString =>
+        "mongodb+srv://moha:Yr2Ab6A2lVO00J0A@cluster0.j4olwzd.mongodb.net/?retryWrites=true&w=majority";
+}
--- MongoDb.cs
+++ MongoDb.cs
@@ -3,7 +3,7 @@
 public class MongoDb
 {
     private static readonly Lazy<MongoDb> _lazyInstance = new Lazy<MongoDb>(() => new MongoDb());
-    private const string connectionString = "mongodb+srv://moha:Yr2Ab6A2lVO00J0A@cluster0.j4olwzd.mongodb.net/?retryWrites=true&w=majority";
+    private string connectionString = Constants.MongoDbConnectionString;
     private MongoClient client;
     private IMongoDatabase database;
     private IMongoCollection<Product> productsCollection;
